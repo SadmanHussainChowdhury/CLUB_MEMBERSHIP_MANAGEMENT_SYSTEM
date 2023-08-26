d2e7623381301659eb2780a0b74eb43e030f2afe
Md Yasin <83154902+mdyasin399@users.noreply.github.com>
2023-08-26 09:22:40 +0600

    Add files via upload

---
+++ admin_profile_pic_post.php
@@ -0,0 +1,50 @@
+
+<?php
+session_start();
+require 'login_check.php';
+require 'db.php';
+
+$userid = $_POST['id'];
+$photo = $_FILES['photo'];
+//print_r($_FILES['photo']);
+
+
+$after_explode = explode('.', $photo['name']);
+$extension = end($after_explode);
+$allowed_extension = array('jpg', 'png', 'gif', 'webp', 'PNG', 'JPG');
+if (in_array($extension, $allowed_extension)) {
+    if ($photo['size'] <= 3000000) {
+
+        //delete previous img starts
+        $select_img = "SELECT * FROM users WHERE id=$userid";
+        $img_res = mysqli_query($db_connection, $select_img);
+        $img_after_assoc = mysqli_fetch_assoc($img_res);
+        $delete_from = 'C:\xampp\htdocs\cmms\uploads\profile_pic/' . $img_after_assoc['photo'];
+        unlink($delete_from);
+        //delete previous img ends
+
+        $file_name = $userid . '.' . $extension;
+        //echo $file_name;
+        // die();
+
+        $new_location = 'C:\xampp\htdocs\cmms\uploads\profile_pic/' . $file_name;
+        move_uploaded_file($photo['tmp_name'], $new_location);
+        $update = "UPDATE users SET photo='$file_name' WHERE id=$userid";
+        mysqli_query($db_connection, $update);
+        header('location:admin_profile.php');
+    } else {
+        $_SESSION['img_error'] = 'File size exceed';
+        header('location:admin_profile.php');
+    }
+} else {
+    $_SESSION['extension'] = 'Invalid extension';
+    header('location:admin_profile.php');
+}
+
+
+
+
+
+
+
+?>
